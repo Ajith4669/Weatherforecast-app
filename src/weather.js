1f199d6cf94efebaafcc4e4f94d476ef182b8969
Ajith <ajithkumar000@gmail.com>
2023-04-12 20:57:05 +0530

    First code commit

---
+++ src/weather.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const apiKey = "f56f24967aaf51182d1d4df628297c6d";
+  const [inputCity, setInputCity] = useState("");
+  const [data, setData] = useState({});
+
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    const apiURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+    axios
+      .get(apiURL)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {});
+  };
+
+  const handleChangeInput = (e) => {
+    console.log("value", e.target.value);
+    setInputCity(e.target.value);
+  };
+
+  const handleSearch = () => {
+    getWeatherDetails(inputCity);
+  };
+
+  return (
+    <div className="col-md-12">
+      <div className="weatherBg">
+        <h1 className="heading">Weather App</h1>
+
+        <div className="d-grid gap-3 col-4 mt-4">
+          <input
+            type="text"
+            placeholder="Enter City"
+            align="center"
+            className="form-control"
+            value={inputCity}
+            onChange={handleChangeInput}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
+        </div>
+      </div>
+
+      {Object.keys(data).length > 0 && (
+        <div className="col-md-12 text-center mt-5">
+          <div className="shadow rounded weatherResultBox">
+            <h3>Report</h3>
+            <img
+              className="weatherIcon"
+              src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png"
+            />
+            <h5 className="weatherCity">City - {data?.name}</h5>
+            <h6 className="weatherTemp">
+              {" "}
+              Temp -{(data?.main?.temp - 273.15).toFixed(2)}°C
+            </h6>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
